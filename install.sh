625244cf91f1fdb1124c89abd2088005c0eadb5e
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 16:45:54 +0300

    edited install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,12 @@ sudo mv EventToInternet/ /etc/systemd/system/
 sudo chown -R root:root /etc/systemd/system/EventToInternet
 cd /etc/systemd/system/EventToInternet
 
-
+sudo git pull
+sudo cp -f /etc/systemd/system/EventToInternet/EventToInternet.service /etc/systemd/system/EventToInternet.service
 
 sudo systemctl daemon-reload
 sudo systemctl stop EventToInternet.service
 sudo systemctl disable EventToInternet.service
-
-sudo git pull
-sudo cp -f /etc/systemd/system/EventToInternet/EventToInternet.service /etc/systemd/system/EventToInternet.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable EventToInternet.service
