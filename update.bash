78a65a632e9f5043729a61ed1096949d6cbd5fe4
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 17:44:06 +0300

    remove while

--- update.bash
+++ update.bash
@@ -1,51 +1,48 @@
 #!/bin/bash
 
-while [[ 1 ]]
-do
+update=False
+
+echo "ping https://github.com"
+case "$(curl -s --max-time 2 -I https://github.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+  [23]) echo "HTTP connectivity is up"; update=True;;
+  5) echo "The web proxy won't let us through";;
+  *) echo "The network is down or very slow";;
+esac
+
+if [[ ${update} == True ]]
+then
     update=False
 
-    echo "ping https://github.com"
-    case "$(curl -s --max-time 2 -I https://github.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
-      [23]) echo "HTTP connectivity is up"; update=True;;
-      5) echo "The web proxy won't let us through";;
-      *) echo "The network is down or very slow";;
-    esac
+    git remote update
 
-    if [[ ${update} == True ]]
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    if [[ ${LOCAL} = ${REMOTE} ]]
     then
-        update=False
-
-        git remote update
-
-        UPSTREAM=${1:-'@{u}'}
-        LOCAL=$(git rev-parse @)
-        REMOTE=$(git rev-parse "$UPSTREAM")
-        BASE=$(git merge-base @ "$UPSTREAM")
-
-        if [[ ${LOCAL} = ${REMOTE} ]]
-        then
-            echo "Up-to-date"
-        elif [[ ${LOCAL} = ${BASE} ]]
-        then
-            echo "Need to pull"
-            update=True
-        elif [[ ${REMOTE} = ${BASE} ]]
-        then
-            echo "Need to push"
-        else
-            echo "Diverged"
-        fi
-    fi
-
-    if [[ ${update} == True ]]
+        echo "Up-to-date"
+    elif [[ ${LOCAL} = ${BASE} ]]
     then
-        git checkout .
-        git clean -fd
-        git pull --force
-        cp -f /etc/systemd/system/EventToInternet/EventToInternet.service /etc/systemd/system/EventToInternet.service
-        systemctl daemon-reload
-        systemctl restart EventToInternet.service
+        echo "Need to pull"
+        update=True
+    elif [[ ${REMOTE} = ${BASE} ]]
+    then
+        echo "Need to push"
+    else
+        echo "Diverged"
     fi
+fi
 
-    sleep 10
-done
+if [[ ${update} == True ]]
+then
+    git checkout .
+    git clean -fd
+    git pull --force
+    cp -f /etc/systemd/system/EventToInternet/EventToInternet.service /etc/systemd/system/EventToInternet.service
+    systemctl daemon-reload
+    systemctl restart EventToInternet.service
+fi
+
+
