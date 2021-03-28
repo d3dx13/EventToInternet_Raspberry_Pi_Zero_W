1e5788699b0a843a8502f19d2d0048f8c27fb688
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 16:14:20 +0300

    Add service

--- install.sh
+++ install.sh
@@ -9,3 +9,9 @@ sudo apt autoremove -y
 sudo apt install -y htop python3 python3-dev python3-pip gcc
 python3 -m pip install -r requirements.txt
 
+git clone https://github.com/ITMO-lab/wifi-autoconnector.git
+sudo mv EventToInternet/ /etc/systemd/system/
+cd /etc/systemd/system/EventToInternet
+
+sudo git pull
+sudo cp -f EventToInternet.service /etc/systemd/system/EventToInternet.service
