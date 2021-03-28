646158afa1ff5ce75f0e2817d10daac19c802e02
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 17:22:10 +0300

    update.bash

--- update.bash
+++ update.bash
@@ -1,2 +1,47 @@
 #!/bin/bash
 
+update=False
+
+echo "ping https://github.com"
+case "$(curl -s --max-time 2 -I https://github.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+  [23]) echo "HTTP connectivity is up"; update=True;;
+  5) echo "The web proxy won't let us through";;
+  *) echo "The network is down or very slow";;
+esac
+
+if [[ ${update} == True ]]
+then
+    update=False
+
+    git remote update
+
+    UPSTREAM=${1:-'@{u}'}
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    if [[ ${LOCAL} = ${REMOTE} ]]
+    then
+        echo "Up-to-date"
+    elif [[ ${LOCAL} = ${BASE} ]]
+    then
+        echo "Need to pull"
+        update=True
+    elif [[ ${REMOTE} = ${BASE} ]]
+    then
+        echo "Need to push"
+    else
+        echo "Diverged"
+    fi
+fi
+
+if [[ ${update} == True ]]
+then
+    git pull --force
+    git checkout .
+    git clean -fd
+fi
+
+
+
+
