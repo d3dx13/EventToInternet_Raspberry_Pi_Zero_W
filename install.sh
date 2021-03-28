46c638ce300fe2cc7b7ab69e967b1688741c06a2
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 14:56:57 +0300

    Обновление инструкций к установке

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
-sudo reboot
+# sudo reboot
 
-sudo apt install -y htop
-sudo apt install -y python3.7 python3.7-dev python3-pip gcc
-python3 -m pip install evdev
-
+sudo apt install -y htop python3 python3-dev python3-pip gcc
+python3 -m pip install -r requirements.txt
 
