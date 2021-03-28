c6e1a0120cff4fe16c8f29c6888786c71804b3ae
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 16:48:06 +0300

    test commit update

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ sudo systemctl daemon-reload
 sudo systemctl stop EventToInternet.service
 sudo systemctl disable EventToInternet.service
 
-
 sudo systemctl daemon-reload
 sudo systemctl enable EventToInternet.service
 sudo systemctl start EventToInternet.service
