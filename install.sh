9f01677902705374bcd6f8737b5b4151e24d213d
d3dx13 <zhidkov2846@gmail.com>
2021-03-28 11:29:41 +0300

    Создание прототипа

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo reboot
+
+sudo apt install -y htop
+sudo apt install -y python3.7 python3.7-dev python3-pip gcc
+python3 -m pip install evdev
+
+
