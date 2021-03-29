8ce9002c90fe6cb233bccbd25aef65113614d53f
d3dx13 <zhidkov2846@gmail.com>
2021-03-29 20:38:24 +0300

    https://webhook.site/#!/79d5f27a-7c16-47b6-97cc-9ba88d423499

--- async_executor.py
+++ async_executor.py
@@ -5,8 +5,7 @@ from EventToInternet.KeyboardListener import KeyboardListener
 
 class BarcodeKeyboardListener(KeyboardListener):
     async def dict_handler(self, dict_event):
-        print(dict_event)
-        requests.post("https://webhook.site/595ddd9f-de34-4af8-845c-c52bb2614083", json=dict_event)
+        requests.post("https://webhook.site/79d5f27a-7c16-47b6-97cc-9ba88d423499", json=dict_event)
 
 
 BarcodeKeyboardListener()
